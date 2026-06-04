376d1a2c163d445f04340bf888b2e54b2112295f
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-06-04 13:07:35 +0600

    Login page design done

--- src/components/modules/authentication/login.tsx
+++ src/components/modules/authentication/login.tsx
@@ -1,10 +1,33 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@base-ui/react"
+
 const Login = () => {
   return (
     <>
-    Login Page
+      <section className="flex justify-center items-center mt-20">
 
-    Name
-    Password
+        <div className="flex w-full max-w-sm min-w-sm flex-col gap-y-4 rounded-md border border-muted bg-background px-6 py-8 shadow-md">
+          <Input
+            type="email"
+            placeholder="Email"
+            className="p-2 border-2 rounded-lg"
+          />
+
+          <Input
+            type="password"
+            placeholder="Password"
+            className="p-2 border-2 rounded-lg"
+          />
+
+          <Button type="submit" className="w-full bg-black text-white cursor-pointer" variant="outline">
+            Login
+          </Button>
+
+
+        </div>
+      </section>
+
+      <p className="flex justify-center mt-4">Need a account? <span className="underline mx-2 cursor-pointer">Sign up</span></p>
     </>
   );
 };
