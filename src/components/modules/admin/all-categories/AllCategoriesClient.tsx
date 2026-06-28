c91aeb61f30d6418d4eccdf7a05154bbf355e250
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-06-28 13:02:10 +0600

    Category Updated

--- src/components/modules/admin/all-categories/AllCategoriesClient.tsx
+++ src/components/modules/admin/all-categories/AllCategoriesClient.tsx
@@ -1,15 +1,33 @@
+import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell } from "@/components/ui/table";
 import { IAllCategoriesClientProps } from "./categories.interface";
+import { Button } from "@/components/ui/button";
 
 export default function ALLCategoriesClient({
-  categories,
+    categories,
 }: IAllCategoriesClientProps) {
-  return (
-    <>
-      {categories.map((category) => (
-        <div key={category.id}>{category.name}</div>
-      ))}
-    </>
-  );
+    return (
+        <>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-center">No</TableHead>
+                        <TableHead className="text-center">Name</TableHead>
+                        <TableHead className="text-center ">Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+
+                <TableBody>
+                    {categories?.map((category, index) => (
+                        <TableRow key={category.id}>
+                            <TableCell className="text-center">{index + 1}</TableCell>
+                            <TableCell className="text-center">{category.name}</TableCell>
+                            <TableCell className="text-center"><Button className="cursor-pointer">Delete</Button></TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </>
+    );
 }
 
 
